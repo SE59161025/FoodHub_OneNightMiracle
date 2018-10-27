385b26adfdfc8e10a47eb28c4c3db5d38db35185
59160643 <59160643@go.buu.ac.th>
2018-10-28 02:38:19 +0700

    sad

--- src/pages/what_eat/what_eat.ts
+++ src/pages/what_eat/what_eat.ts
@@ -1,17 +1,41 @@
-import { NavController } from 'ionic-angular';
 import { Component } from '@angular/core';
+import { list_eat } from '../list_eat/list_eat';
+import { NavController } from 'ionic-angular';
+import { FoodListProvider } from '../../providers/food-list/food-list';
 import { MenuPage } from '../menu/menu';
 
-
 @Component({
-  selector: 'what-eat-page',
+  selector: 'what_eat-page',
   templateUrl: 'what_eat.html'
 })
 export class what_eat{
-    constructor(public navCtrl:NavController) {
+  toppings : string;
+  checkvalidate:boolean=false;
+  constructor(public navCtrl:NavController,private _FoodListProvider:FoodListProvider) {
+    console.log(_FoodListProvider.test);
+    console.log(_FoodListProvider.food_list);
+  }
+  pushPage(){
+    this.navCtrl.push(list_eat, {
+      'toppings': this.toppings
+    });
+  }
+  pushMenu(){
+    this.navCtrl.push(MenuPage, {
+    });
+  }
+  not(data){
+    console.log('ทดสอบ ',data);
+    if(data.length!=0){
+      this.checkvalidate=true;
+    }else{
+      this. checkvalidate=false;
     }
-    pushMenu(){
-      this.navCtrl.push(MenuPage, {
-      });
-    }
+  }
+  not2(){
+    console.log('ทดสอบบบบบบบบบบบบบบบ');
+  }
+  showSelectValue = function(mySelect) {
+    console.log(mySelect);
+}
 }
