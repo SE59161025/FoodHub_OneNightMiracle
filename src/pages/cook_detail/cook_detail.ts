c364958b30f39b53f9b31e4f4651684597266fcb
59160643 <59160643@go.buu.ac.th>
2018-10-27 20:16:04 +0700

    edit type_food by somlag

--- src/pages/cook_detail/cook_detail.ts
+++ src/pages/cook_detail/cook_detail.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { NavParams } from 'ionic-angular';
 @Component({
   selector: 'cook-detail',
   templateUrl: 'cook_detail.html'
 })
-export class cook_detail {
- 
-  constructor() {
-      
+export class cook_detail implements OnInit {
+    name;
+    link_img;
+    solution;
+    staple;
+    level;
+    time_solu;
+    time_sta;
+  constructor(private navParams: NavParams) {
+    this.name = navParams.get('name');
+    this.link_img = navParams.get('link_img'); 
+    this.solution = navParams.get('solution');
+    this.staple = navParams.get('staple');
+    this.level  = navParams.get('level');
+    this.time_solu = navParams.get('time_solu');
+    this.time_sta  = navParams.get('time_sta');
   }
  
+  ngOnInit(){
+    console.log(this.solution);
+  }
 }
