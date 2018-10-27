cde0d37c9fa5f8c97d52974bc27ad2cb9714f299
59160643 <59160643@go.buu.ac.th>
2018-10-28 05:00:47 +0700

    sad

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'contact',
   templateUrl: 'contact.html'
 })
+
 export class contact {
    
   constructor() {
-    
+
   }
- 
+
 }
